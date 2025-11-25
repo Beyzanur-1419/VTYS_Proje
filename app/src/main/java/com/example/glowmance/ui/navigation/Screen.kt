588a4ecbe8f30f9dddf45f7c77fb801d86312eaa
Tuğba BARS <147241119+tugbars13@users.noreply.github.com>
2025-11-25 11:01:35 +0300

    Delete app directory

--- app/src/main/java/com/example/glowmance/ui/navigation/Screen.kt
+++
@@ -1,8 +0,0 @@
-package com.example.glowmance.ui.navigation
-
-sealed class Screen(val route: String) {
-    object Welcome : Screen("welcome")
-    object SignIn : Screen("signin")
-    object SignUp : Screen("signup")
-    object Home : Screen("home")
-}
