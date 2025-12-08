39b21cdb437f03dc6ca8bdc16602f3e57c6237a9
Tuğba BARS <147241119+tugbars13@users.noreply.github.com>
2025-12-08 17:15:45 +0300

    Delete Frontend directory

--- Frontend/app/src/main/java/com/example/glowmance/ui/navigation/Screen.kt
+++
@@ -1,9 +0,0 @@
-package com.example.glowmance.ui.navigation
-
-sealed class Screen(val route: String) {
-    object Welcome : Screen("welcome")
-    object SignIn : Screen("sign_in")
-    object SignUp : Screen("sign_up")
-    object Home : Screen("home")
-    object ForgotPassword : Screen("forgot_password")
-}
